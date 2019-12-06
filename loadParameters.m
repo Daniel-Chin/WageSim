54b62aa9c1bbf39f5b6a4c5ab1c3183204590c85
Daniel Chin <daniel_chin@yahoo.com>
2019-12-06 15:38:10 -0500

    Change parameters

--- loadParameters.m
+++ loadParameters.m
@@ -1,12 +1,12 @@
 % All global parameters
 
 N_GOOD = 10;
-N_PROCESS = 100;
+N_PROCESS = 1000;
 CAPITAL_GOOD_PROPORTION = .5;
 DEPRECIATION = .9;  % capital good depreciation
-BLESS = 0.7;  % scales the product of all processes. Kinda like the "difficulty level" of the world.
-MINIMA_QUALITY = .0000001;  % quality of Stride Descend
-R_STICKY = [.9 .1]';
+BLESS = 0.5;  % scales the product of all processes. Kinda like the "difficulty level" of the world.
+R_STICKY = [.8 .2]';
+LOSS_TOLERANCE = .0001;
 
 n_capital_good = round(N_GOOD * CAPITAL_GOOD_PROPORTION);
 n_consumer_good = N_GOOD - n_capital_good;
