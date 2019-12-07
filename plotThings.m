934bc66340fcb5302d62ecd998128130ac48b1c7
Daniel Chin <daniel_chin@yahoo.com>
2019-12-07 00:37:43 -0500

    betterPlot

--- plotThings.m
+++ plotThings.m
@@ -3,11 +3,11 @@ SHIFT = 3;
 
 hold off;
 hh = [];
-hh(1) = plot(sum_r_history + SHIFT, 'DisplayName', 'sum(r)', 'linewidth', LINEWIDTH);
+hh(4+1:4+N_GOOD) = area(p_history);
 hold on;
+hh(1) = plot(sum_r_history + SHIFT, 'DisplayName', 'sum(r)', 'linewidth', LINEWIDTH);
 hh(2) = plot(capital_GDP_history + SHIFT, 'DisplayName', 'capital GDP', 'linewidth', LINEWIDTH);
 hh(3) = plot(consumer_GDP_history + SHIFT, 'DisplayName', 'consumer GDP', 'linewidth', LINEWIDTH);
 hh(4) = plot(structural_change_history * 10 + 1, 'DisplayName', 'structural change', 'linewidth', LINEWIDTH);
-hh(4+1:4+N_GOOD) = area(p_history);
 legend(hh(1:4), 'Location', 'northwest');
-axis([0 length(sum_r_history) 0 5]);
+axis([0, length(sum_r_history), 0, SHIFT+max(2, ceil(max(sum_r_history) / 3) * 3)]);
