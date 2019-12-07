acde8788502ef4a0c79953d0b7172a017241de0b
Daniel Chin <daniel_chin@yahoo.com>
2019-12-07 10:55:17 -0500

    No more random walk

--- loadParameters.m
+++ loadParameters.m
@@ -6,7 +6,6 @@ CAPITAL_GOOD_PROPORTION = .5;
 DEPRECIATION = .9;  % capital good depreciation
 BLESS = 0.5;  % scales the product of all processes. Kinda like the "difficulty level" of the world.
 R_STICKY = [.9 .1]';
-RANDOM_WALK_STRIDE = .05;
 LOSS_TOLERANCE = .0001;
 
 n_capital_good = round(N_GOOD * CAPITAL_GOOD_PROPORTION);
