bafd04e1d83b00ca2acd43bd40c9725bcfcb2bd4
Daniel Chin <daniel_chin@yahoo.com>
2019-12-07 10:27:06 -0500

    betterPlot

--- plotThings.m
+++ plotThings.m
@@ -1,13 +1,27 @@
 LINEWIDTH = 1.5;
-SHIFT = 3;
 
-hold off;
+plot_upper_bound = ceil(max([ ...
+  max(sum_r_history), ...
+  max(capital_GDP_history), ...
+  max(consumer_GDP_history)] ...
+));
+
+plot_lower_bound = floor(min([ ...
+  min(sum_r_history), ...
+  min(capital_GDP_history), ...
+  min(consumer_GDP_history)] ...
+));
+
+plot_ratio = 1 / (plot_upper_bound - plot_lower_bound);
+transformPlot = @(x)((x - plot_lower_bound) * plot_ratio + 1);
+
 hh = [];
+hold off;
 hh(4+1:4+N_GOOD) = area(p_history);
 hold on;
-hh(1) = plot(sum_r_history + SHIFT, 'DisplayName', 'sum(r)', 'linewidth', LINEWIDTH);
-hh(2) = plot(capital_GDP_history + SHIFT, 'DisplayName', 'capital GDP', 'linewidth', LINEWIDTH);
-hh(3) = plot(consumer_GDP_history + SHIFT, 'DisplayName', 'consumer GDP', 'linewidth', LINEWIDTH);
-hh(4) = plot(structural_change_history * 10 + 1, 'DisplayName', 'structural change', 'linewidth', LINEWIDTH);
+hh(1) = plot(transformPlot(sum_r_history), 'b', 'DisplayName', 'sum(r)', 'linewidth', LINEWIDTH);
+hh(2) = plot(transformPlot(capital_GDP_history), 'r', 'DisplayName', 'capital GDP', 'linewidth', LINEWIDTH);
+hh(3) = plot(transformPlot(consumer_GDP_history), 'g', 'DisplayName', 'consumer GDP', 'linewidth', LINEWIDTH);
+hh(4) = plot(structural_change_history * 10 + 1, 'k--', 'DisplayName', 'structural change', 'linewidth', LINEWIDTH);
 legend(hh(1:4), 'Location', 'northwest');
-axis([0, length(sum_r_history), 0, SHIFT+max(2, ceil(max(sum_r_history) / 3) * 3)]);
+axis([0, length(sum_r_history), 0, 2]);
