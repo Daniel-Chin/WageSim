8e5a7ad8bb196da41acd57971b95024154bd9e68
Daniel Chin <daniel_chin@yahoo.com>
2019-12-07 10:10:09 -0500

    plot according to real time

--- test.m
+++ test.m
@@ -1,6 +1,6 @@
 close all;
 
-BATCH = 2;
+SEC_PER_FRAME = 2;
 
 initRP();
 p_history = [];
@@ -8,13 +8,16 @@ sum_r_history = [];
 capital_GDP_history = [];
 consumer_GDP_history = [];
 structural_change_history = [];
-for test_i = 1 : test_i_max / BATCH
-  p_array = zeros(BATCH, N_GOOD);
-  sum_r_array = zeros(BATCH, 1);
-  capital_GDP_array = zeros(BATCH, 1);
-  consumer_GDP_array = zeros(BATCH, 1);
-  structural_change_array = zeros(BATCH, 1);
-  for batch_i = 1 : BATCH
+last_plot_time = now;
+p_array = zeros(BATCH, N_GOOD);
+sum_r_array = zeros(BATCH, 1);
+capital_GDP_array = zeros(BATCH, 1);
+consumer_GDP_array = zeros(BATCH, 1);
+structural_change_array = zeros(BATCH, 1);
+for test_i = 1 : test_i_max
+  batch_i = 0;
+  while now - last_plot_time < SEC_PER_FRAME * .00001
+    batch_i = batch_i + 1;
     p_array(batch_i, :) = p;
     sum_r_array(batch_i) = log(sum(r));
     value = sum(supply .* p, 1);
@@ -26,13 +29,14 @@ for test_i = 1 : test_i_max / BATCH
     structural_change_array(batch_i) = norm(r/norm(r) - last_r/norm(last_r));
   end
   fprintf('concat...');
-  p_history = cat(1, p_history, p_array);
-  sum_r_history = cat(1, sum_r_history, sum_r_array);
-  capital_GDP_history = cat(1, capital_GDP_history, capital_GDP_array);
-  consumer_GDP_history = cat(1, consumer_GDP_history, consumer_GDP_array);
-  structural_change_history = cat(1, structural_change_history, structural_change_array);
+  p_history = cat(1, p_history, p_array(1:batch_i, :));
+  sum_r_history = cat(1, sum_r_history, sum_r_array(1:batch_i));
+  capital_GDP_history = cat(1, capital_GDP_history, capital_GDP_array(1:batch_i));
+  consumer_GDP_history = cat(1, consumer_GDP_history, consumer_GDP_array(1:batch_i));
+  structural_change_history = cat(1, structural_change_history, structural_change_array(1:batch_i));
   fprintf('plot...');
   plotThings();
+  last_plot_time = now;
   fprintf('done.\n');
   % disp('Press Enter...');
   % pause
